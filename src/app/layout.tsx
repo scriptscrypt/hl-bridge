f0c32d54b1adc3e97dffbc8ac64b9deeff25115c
Scriptscrypt <tirumaladasa@gmail.com>
2024-12-27 01:54:01 +0530

    Feat: analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import {
   redactionBold,
   redactionItalic,
 } from "@/assets/fonts/fonts-custom";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -50,6 +51,7 @@ export default function RootLayout({
         className={`${inter.className} ${redaction.variable} ${redactionBold.variable} ${redactionItalic.variable} `}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
